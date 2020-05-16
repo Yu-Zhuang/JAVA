c85265c15492c0237fad679f9bbfcfac2c62b874
mild-guy <106711129@gms.tcu.edu.tw>
2020-05-16 22:08:56 +0800

    update

--- JustPractice/HowManyPrime.java
+++ JustPractice/HowManyPrime.java
@@ -1,8 +1,6 @@
 import java.util.*;
 /*
-題目：古典問題：有一對兔子，
-從出生後第3個月起每個月都生一對兔子，
-小兔子長到第三個月後每個月又生一對兔子，假如兔子都不死，問每個月的兔子對數為多少？
+題目：判斷兩數間有幾個Prime
 */
 class Solution{
     int HowManyPrime(int min, int max){
